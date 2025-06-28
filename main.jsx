0e08bbe4ae119a4c585ce724a4d26818b5eac7ef
YoutubeVieira <famillevieiradossantos51@gmail.com>
2025-06-28 07:13:30 +0200

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RemotionRoot } from './VideoComposition';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RemotionRoot />
+  </React.StrictMode>,
+);
